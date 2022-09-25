8327d9011cbe64cd5454676f11ce8d8b90b3c2b1
WillGreen2 <will.green98@hotmail.com>
2022-09-25 20:32:41 +0100

    7 - M (Solution)

---
+++ Cpp/Medium/007-ReverseInteger.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+    public:
+        int reverse(int x) {
+            int reverse = 0;
+
+            while(x != 0) {
+                int digit = x % 10;
+                x /= 10;
+
+                if(reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && digit > 7)) return 0;
+                if(reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && digit < -8)) return 0;
+
+                reverse = reverse * 10 + digit;
+            }
+
+            return (int)reverse;
+        }
+};
+// @lc code=end
