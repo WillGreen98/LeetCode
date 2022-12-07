ef32c6bbf89987e980486c60bcf0f50fee9515f1
WillGreen98 <will.green98@hotmail.com>
2022-12-07 10:33:42 +0000

    938 - E

---
+++ Cpp/Easy/938-RangeSumOfBst.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=938 lang=cpp
+ *
+ * [938] Range Sum of BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        int rangeSumBST(TreeNode *root, int low, int high) {
+            if(root == nullptr) return 0;
+            if(root->val < low) return rangeSumBST(root->right, low, high);
+            if(root->val > high) return rangeSumBST(root->left, low, high);
+
+            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+        }
+};
+// @lc code=end
