500d504b2d61f9f08b54f3a8c5afceef4750918d
WillGreen98 <will.green98@hotmail.com>
2023-01-14 23:59:43 +0000

    1061 - M

--- Cpp/Medium/1061-LexicographicallySmallestEquivalentString.cpp
+++ Cpp/Medium/1061-LexicographicallySmallestEquivalentString.cpp
@@ -1,15 +1,18 @@
 class Solution {
     public:
         string smallestEquivalentString(string s1, string s2, string baseStr) {
-            for (int i = 0; i < s1.size(); i++) {
-            char toReplace = max(s1[i], s2[i]);
-            char replaceWith = min(s1[i], s2[i]);
-            for (int j = 0; j < baseStr.size(); j++) {
-                if (baseStr[j] == toReplace) {
-                    baseStr[j] = replaceWith;
-                }
+            char ch[26];
+
+            for(int i = 0; i < 26; i++) ch[i] = 'a' + i;
+            for(int i = 0; i < s1.size(); i++) {
+                char toReplace = max(ch[s1[i] - 'a'], ch[s2[i] - 'a']);
+                char replaceWith = min(ch[s1[i] - 'a'], ch[s2[i] - 'a']);
+
+                for(int i = 0; i < 26; i++) if(ch[i] == toReplace) ch[i] = replaceWith;
             }
+
+            for(int i = 0; i < baseStr.size(); i++) baseStr[i] = ch[baseStr[i] - 'a'];
+
+            return baseStr;
         }
-        return baseStr;
-    }
 };
