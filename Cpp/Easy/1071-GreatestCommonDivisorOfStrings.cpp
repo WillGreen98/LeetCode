615f9f429cc9274696a9bbb9f9d38271a30ba4fa
WillGreen98 <will.green98@hotmail.com>
2023-02-01 08:52:32 +0000

    1071 - E

---
+++ Cpp/Easy/1071-GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1071 lang=cpp
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+// @lc code=start
+class Solution {
+    public:
+        string gcdOfStrings(string str1, string str2) {
+            return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.size(), str2.size())) : "";
+        }
+};
+// @lc code=end
