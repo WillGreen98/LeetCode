d2e91ec529870576de79a3929053510707c57be0
WillGreen98 <will.green98@hotmail.com>
2023-05-10 20:34:15 +0100

    Having some fun

--- Cpp/Easy/1572-MatrixDiagonalSum.cpp
+++ Cpp/Easy/1572-MatrixDiagonalSum.cpp
@@ -11,6 +11,19 @@ class Solution {
             return accumulate(mat.begin(), mat.end(), 0, [&](int sum, vector<int> &diag) {
                 return sum + static_cast<int>(diag[&diag - &mat[0]] + diag[&mat[0] + diag.size() - 1 - &diag]);
             }) - (mat.size() % 2 == 0 ? 0 : mat[mat.size() / 2][mat.size() / 2]);
+
+            // equates to
+
+            // if(mat.empty()) return 0;
+
+            // int sum = 0;
+            // for(int i = 0; i < mat.size(); i++) {
+            //     sum += mat[i][i] + mat[i][mat.size() - 1 - i];
+            // }
+
+            // if(mat.size() % 2 == 1) sum -= mat[mat.size() / 2][mat.size() / 2];
+
+            // return sum;
         }
 };
 // @lc code=end
