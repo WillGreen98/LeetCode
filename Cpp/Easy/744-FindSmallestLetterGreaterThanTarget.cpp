6a6a2639da61ada8787cdc8cc8d3553539b9393d
WillGreen98 <will.green98@hotmail.com>
2023-06-09 22:01:35 +0100

    744 - E

---
+++ Cpp/Easy/744-FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=744 lang=cpp
+ *
+ * [744] Find Smallest Letter Greater Than Target
+ */
+
+// @lc code=start
+class Solution {
+    public:
+        char nextGreatestLetter(vector<char>& letters, char target) {
+            int left = 0;
+            int right = letters.size() - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (letters[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return letters[left % letters.size()];
+        }
+};
+// @lc code=end
