0188631e8f929187f2babc558994887cb4980a53
WillGreen98 <will.green98@hotmail.com>
2024-01-20 23:31:57 +0000

    907 - M

---
+++ Cpp/Medium/907-SumOfSubarrayMinimums.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=907 lang=cpp
+ *
+ * [907] Sum of Subarray Minimums
+ */
+
+// @lc code=start
+class Solution {
+    private:
+        const int MOD = 1e9 + 7;
+    public:
+        int sumSubarrayMins(vector<int> &arr) {
+            int n = arr.size();
+
+            vector<int> left(n);
+            vector<int> right(n);
+
+            stack<int> st;
+
+            long long result = 0;
+
+            for(int i = 0; i < n; ++i) {
+                while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
+
+                left[i] = st.empty() ? -1 : st.top();
+                st.push(i);
+            }
+
+            while(!st.empty()) st.pop();
+
+            for(int i = n - 1; i >= 0; --i) {
+                while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+
+                right[i] = st.empty() ? n : st.top();
+                st.push(i);
+            }
+
+            for(int i = 0; i < n; ++i) result = ((result + (long long)arr[i] * (i - left[i]) * (right[i] - i)) % MOD);
+
+            return static_cast<int>(result);
+        }
+};
+// @lc code=end
